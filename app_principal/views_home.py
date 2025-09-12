a3f78e001f043aaf49ac7c26fce76a0d80f5af92
Homero <sistemaservidor254@gmail.com>
2025-09-12 16:13:22 -0300

    Centraliza home.html na pasta global de templates

--- app_principal/views_home.py
+++
@@ -1,7 +0,0 @@
-# app_principal/views_home.py
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
-
-
-def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "home.html")
